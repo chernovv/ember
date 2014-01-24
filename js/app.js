934be34c0d95d54e817b38a2071c9c50d7221546
Vasiliy Chernov <vchernov@geeksforless.net>
2014-01-24 18:27:34 +0200

    all works (all in handlebar)

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
 window.Todos = Ember.Application.create();
-=======
-window.Todos = Ember.Application.Extend();
->>>>>>> 06c50701eb8fb8adf9fa2b8e0f5931162f63bd1f
+
+//window.Todos = Ember.Application.extend();
+
