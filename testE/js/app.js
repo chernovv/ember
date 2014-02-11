1e114993ec928a75666df97d091270606c549c5a
chernovv <vchernov@geeksforless.net>
2014-02-11 02:58:29 +0200

    search

--- testE/js/app.js
+++ testE/js/app.js
@@ -1,11 +1,55 @@
-App = Ember.Application.create();
+/*	Application*/
+App = Em.Application.create();
 
-App.Router.map(function() {
-  // put your routes here
+/*	MODEL	*/
+
+/*	CONTROLLER	*/
+App.tweetsController = Em.ArrayController.create({
+    content: [],
+    username: '',
+    loadTweets: function() {
+        var me = this;
+        var username = me.get("username");
+        //alert(username);
+        if ( username ) {
+            var url = 'http://api.twitter.com/1/statuses/user_timeline.json'
+                url += '?screen_name=%@&callback=?'.fmt(me.get("username"));
+            // push username to recent user array
+            App.recentUsersController.addUser(username);
+        }
+    }
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+App.recentUsersController = Em.ArrayController.create({
+    content: [],
+    addUser: function(name) {
+        if ( this.contains(name) ) this.removeObject(name);
+        this.pushObject(name);
+    },
+    removeUser: function(view){
+        this.removeObject(view.context);
+    },
+    searchAgain: function(view){
+        App.tweetsController.set('username', view.context);
+        App.tweetsController.loadTweets();
+    },
+    reverse: function(){
+        return this.toArray().reverse();
+    }.property('@each')
 });
+
+/*	VIEW	*/
+App.SearchTextField = Em.TextField.extend({
+    insertNewline: function(){
+         App.tweetsController.loadTweets();
+    }
+});
+
+
+
+
+
+
+
+
+
